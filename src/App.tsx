adb1f3be0333759eea27700f5bbe9efb87734ed5
Tien Tran <tientran@Tiens-MacBook-Pro.local>
2023-05-21 18:39:00 -0700

    Fix desktop nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  theme,
-  Box
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import Navbar from "./Navbar"
+import * as React from 'react';
+import { ChakraProvider, theme, Box } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Navbar from './Navbar';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Navbar/>
-    <ColorModeSwitcher/>    
+    <Navbar />
+    <ColorModeSwitcher />
     {/* <Box w="100%" h="200px" bgGradient="linear(to-t, green.200, pink.500)" /> */}
-
   </ChakraProvider>
-)
+);
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,30 +7,66 @@ import {
   Tab,
   Text,
   Center,
+  Flex,
+  chakra,
+  background,
 } from '@chakra-ui/react';
+
 const Navbar = () => {
   // Name text
   // Navbar style
   // Contact Text
   return (
-    <HStack bg="white" w="100%"  color="black">
-      <HStack >
-        <Text display={'inline'}>Tien Tran</Text>
-      </HStack>
-      {/* <Center w={'100%'}> */}
-        <Tabs variant={'soft-rounded'} colorScheme={'blackAlpha'}>
-          <TabList>
-            <Tab>Home</Tab>
-            <Tab>About</Tab>
-            <Tab>Bookmarks</Tab>
-            <Tab>Projects</Tab>
+    <chakra.header id="header">
+      <Flex w={'100%'} py={4} px={4} justify={'space-between'}>
+        <Center>
+          <Text display={'inline'}>Tien Tran</Text>
+        </Center>
+        {/* <Center w={'100%'}> */}
+        <Tabs
+          variant={'soft-rounded'}
+          // colorScheme={'whiteAlpha'}
+        >
+          <TabList bgColor={'blackAlpha.100'} py={1} px={1} borderRadius={24}>
+            <Tab
+              _selected={{
+                textColor: 'black',
+                bg: 'white',
+              }}
+            >
+              Home
+            </Tab>
+            <Tab
+              _selected={{
+                textColor: 'black',
+                bg: 'white',
+              }}
+            >
+              About
+            </Tab>
+            <Tab
+              _selected={{
+                textColor: 'black',
+                bg: 'white',
+              }}
+            >
+              Bookmarks
+            </Tab>
+            <Tab
+              _selected={{
+                textColor: 'black',
+                bg: 'white',
+              }}
+            >
+              Projects
+            </Tab>
           </TabList>
         </Tabs>
-      {/* </Center> */}
-      <HStack>
-        <Text>Contact</Text>
-      </HStack>
-    </HStack>
+        <Center>
+          <Text>Contact</Text>
+        </Center>
+      </Flex>
+    </chakra.header>
   );
 };
 
